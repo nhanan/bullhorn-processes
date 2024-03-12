0b1e60db64df95fa294de46b33cd780525194483
nhanan <65924374+nhanan@users.noreply.github.com>
2024-03-12 09:22:45 +0000

    If Client Contact has this, then add that to contact's Organisation record

---
+++ client-contact-to-org-inherit-value.py
@@ -0,0 +1,49 @@
+from auth import start 
+import requests
+
+swimlane = SWIMLANE
+corp_id = CORP_ID
+rest_url = f"https://rest{swimlane}.bullhornstaffing.com/rest-services/{corp_id}"
+BhRestToken = start()
+
+# Find all client contacts with "Salesforce App Cloud" as a Skill
+def get_client_contacts_with_field_value():
+    start = 0
+    url = f"{rest_url}/query/ClientContact?fields=id,clientCorporation,skills&sort=-id&where=skills.name='Salesforce App Cloud'&count=200&start={str(start)}"
+    headers = {"BhRestToken":BhRestToken}
+    response = requests.request("GET", url, headers=headers).json()
+    num_of_results = len(response['data'])
+
+    while num_of_results > 1:
+        url = f"{rest_url}/query/ClientContact?fields=id,clientCorporation,skills&sort=-id&where=skills.name='Salesforce App Cloud'&count=200&start={str(start)}"
+        headers = {"BhRestToken":BhRestToken}
+        response = requests.request("GET", url, headers=headers).json()
+        start += 200
+        num_of_results = len(response['data'])
+        for dm in response['data']:
+            print(dm['id'])
+            bhid = str(dm['clientCorporation']['id'])
+            add_org_cloud(bhid,"Salesforce App Cloud")
+    else:
+        pass
+
+# Add new value ("Salesforce App Cloud") to "customTextBlock3" field on client contact's org
+def add_org_cloud(bhid, new_cloud):
+    url = f"{rest_url}/entity/ClientCorporation/{bhid}?fields=id,customTextBlock3" #customTextBlock3 is new field to write into
+    headers = {"BhRestToken":BhRestToken}
+    data = requests.request("GET", url, headers=headers).json()
+    # print(data)
+    cloud_list = data['data']['customTextBlock3']
+    if cloud_list is None:
+        cloud_list = new_cloud
+        payload = {"customTextBlock3" : cloud_list}
+        data = requests.request("POST", url, headers=headers, json=payload)
+        print(data.json())
+    else:
+        if new_cloud not in cloud_list:
+            cloud_list.append(new_cloud)
+            payload = {"customTextBlock3" : cloud_list}
+            data = requests.request("POST", url, headers=headers, json=payload)
+            print(data.json())
+
+get_client_contacts_with_field_value()
