440f1e04aeb0a3244d85d04614522b22ddc1bc19
nhanan <65924374+nhanan@users.noreply.github.com>
2024-03-12 11:57:13 +0000

    Update client-contact-to-org-inherit-value.py

--- client-contact-to-org-inherit-value.py
+++ client-contact-to-org-inherit-value.py
@@ -5,6 +5,8 @@ swimlane = SWIMLANE
 corp_id = CORP_ID
 rest_url = f"https://rest{swimlane}.bullhornstaffing.com/rest-services/{corp_id}"
 BhRestToken = start()
+headers = {"BhRestToken":BhRestToken}
+
 
 # Find all client contacts with "Salesforce App Cloud" as a Skill
 def get_client_contacts_with_field_value():
@@ -14,9 +16,8 @@ def get_client_contacts_with_field_value():
     response = requests.request("GET", url, headers=headers).json()
     num_of_results = len(response['data'])
 
-    while num_of_results > 1:
+    while num_of_results > 199:
         url = f"{rest_url}/query/ClientContact?fields=id,clientCorporation,skills&sort=-id&where=skills.name='Salesforce App Cloud'&count=200&start={str(start)}"
-        headers = {"BhRestToken":BhRestToken}
         response = requests.request("GET", url, headers=headers).json()
         start += 200
         num_of_results = len(response['data'])
@@ -25,12 +26,18 @@ def get_client_contacts_with_field_value():
             bhid = str(dm['clientCorporation']['id'])
             add_org_cloud(bhid,"Salesforce App Cloud")
     else:
-        pass
-
+        url = f"{rest_url}/query/ClientContact?fields=id,clientCorporation,skills&sort=-id&where=skills.name='Salesforce App Cloud'&count=200&start={str(start)}"
+        response = requests.request("GET", url, headers=headers).json()
+        num_of_results = len(response['data'])
+        for dm in response['data']:
+            print(dm['id'])
+            bhid = str(dm['clientCorporation']['id'])
+            add_org_cloud(bhid,"Salesforce App Cloud")
+        return
+            
 # Add new value ("Salesforce App Cloud") to "customTextBlock3" field on client contact's org
 def add_org_cloud(bhid, new_cloud):
     url = f"{rest_url}/entity/ClientCorporation/{bhid}?fields=id,customTextBlock3" #customTextBlock3 is new field to write into
-    headers = {"BhRestToken":BhRestToken}
     data = requests.request("GET", url, headers=headers).json()
     # print(data)
     cloud_list = data['data']['customTextBlock3']
